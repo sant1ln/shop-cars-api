0dde1eff28ce6449ce70d30e4ff29e1fd9fac5a9
Santiago s-code2 <santiagoln09@gmail.com>
2023-04-17 18:12:11 -0500

    🚧 creation of first endpoints for clients

--- routers/clients.py
+++ routers/clients.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
 from fastapi.responses import JSONResponse
 from mocks.clients import clients
 
@@ -8,3 +8,16 @@ route_tag = 'clients'
 @client_router.get('/clients', tags=[route_tag])
 def get_clients():
   return JSONResponse(status_code = 200, content = clients)
+
+@client_router.get('/clients/{id}', tags=[route_tag])
+def get_client(id : int = Path(ge=1, le=2000)):
+  for item in clients:
+    print(type(id))
+    print(type(item["id"]))
+    if item["id"] == id:
+      return JSONResponse(content=item)
+    return JSONResponse(status_code=404, content=[])
+
+@client_router.post('/clients', tags=[route_tag])
+def create_client(client):
+  pass
