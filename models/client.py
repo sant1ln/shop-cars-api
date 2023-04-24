063d1c9661bf3299eb88de6886437512845db0af
Santiago s-code2 <santiagoln09@gmail.com>
2023-04-24 18:06:06 -0500

    Temporal fix in bug with JSON data

--- models/client.py
+++ models/client.py
@@ -1,2 +1,9 @@
-class Movie():
-    pass
+from pydantic import BaseModel
+
+class Client(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    client_car: str
+    date: str
+    
--- routers/clients.py
+++ routers/clients.py
@@ -1,23 +1,26 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Body
 from fastapi.responses import JSONResponse
 from mocks.clients import clients
+from models.client import Client
+from typing import Optional, List
 
 client_router = APIRouter()
 route_tag = 'clients'
 
 @client_router.get('/clients', tags=[route_tag])
 def get_clients():
-  return JSONResponse(status_code = 200, content = clients)
+  return { "status_code" : 200, "content" : clients}
 
-@client_router.get('/clients/{id}', tags=[route_tag])
+""" @client_router.get('/clients/{id}', tags=[route_tag])
 def get_client(id : int = Path(ge=1, le=2000)):
   for item in clients:
     print(type(id))
     print(type(item["id"]))
     if item["id"] == id:
       return JSONResponse(content=item)
-    return JSONResponse(status_code=404, content=[])
+    return JSONResponse(status_code=404, content=[]) """
 
-@client_router.post('/clients', tags=[route_tag])
-def create_client(client):
-  pass
+@client_router.post('/clients', tags=[route_tag], response_model=Client)
+def create_client(new_client: Client = Body(...)):
+  clients.insert(0,new_client)
+  return JSONResponse(status_code = 201, content = { "Message" : "Cliente creado con éxito" }) 
